bf1b64bbeb4e24d8c77886b41565267490addff2
Jeroen van Schie <jeroen_van_schie@hotmail.com>
2018-11-14 15:22:18 +0100

    Implemented Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     public function homepage()
     {
@@ -14,6 +15,18 @@ class ArticleController
 
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show one article: %s', $slug));
+        $comments = [
+            'This is awesome!',
+            'So good!',
+            'This is a never ending story.'
+        ];
+
+        return $this->render(
+            'article/show.html.twig',
+            [
+                'title' => ucwords(str_replace('-', ' ', $slug)),
+                'comments' => $comments
+            ]
+        );
     }
 }
