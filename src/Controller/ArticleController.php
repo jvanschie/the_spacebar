9526d705b4c35a1ebaac330fd33345aac2e5025a
Jeroen van Schie <jeroen_van_schie@hotmail.com>
2018-11-19 16:38:09 +0100

    Used the cache service to cache markdown.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Michelf\MarkdownInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,11 +22,15 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/news/{slug}", name="article_show")
+     *
      * @param string $slug
      * @param MarkdownInterface $markdown
+     * @param AdapterInterface $cache
+     *
      * @return Response
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function show(string $slug, MarkdownInterface $markdown): Response
+    public function show(string $slug, MarkdownInterface $markdown, AdapterInterface $cache): Response
     {
         $comments = [
             'This is awesome!',
@@ -37,7 +42,7 @@ class ArticleController extends AbstractController
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-turkey shank eu pork belly meatball non cupim.
+**turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
@@ -51,8 +56,23 @@ strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lo
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
 EOF;
-        $articleContent = $markdown->transform($articleContent);
-        
+
+        // instantiate CacheItem object
+        $item = $cache->getItem('markdown_' . md5($articleContent));
+
+        // if not in cache
+        if (!$item->isHit()) {
+            // transform markdown to html
+            $articleContent = $markdown->transform($articleContent);
+            // set html in cache
+            $item->set($articleContent);
+            // cache html
+            $cache->save($item);
+        }
+
+        // fetch markdown from cache
+        $articleContent = $item->get();
+
         return $this->render(
             'article/show.html.twig',
             [
